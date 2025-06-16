52479ed74f31a4e8cb698cf86235a5b6c1d7e240
Ragini-gillela <graginireddy@gmail.com>
2025-06-16 20:40:26 +0530

    Fibanacci&Palindrome

---
+++ Fibanaci/Fibanaci/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Progarm
+{
+    public static void Main()
+    {
+        //Console.Write("Please enter the number: ");
+        //int n = int.Parse(Console.ReadLine());
+
+        //// Call the Fibonacci function and display the result
+        //int result = FibSeries(n);
+        //Console.WriteLine($"Fibonacci number at position {n} is: {result}");
+        int num=20;
+        string text="test";
+        Console.WriteLine($"Number: {num}, Text: {text}");
+        GetValue(out num, out text);
+        int number = 10;
+        string text1 = "Hello, World!";
+        Console.WriteLine($"Number: {number}, Text: {text1}");
+        ModifyValues(ref number, ref text1);
+        
+    }
+    public static int FibSeries(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+      return FibSeries(n - 1) + FibSeries(n - 2);
+    }
+
+    public static void GetValue(out int x, out string y)
+    {
+        x = 10;
+        y = "Hello";
+        Console.WriteLine($"Number: {x}, Text: {y}");
+    }
+                     
+  public static void ModifyValues(ref int number, ref string text)
+    {
+        number = 20;
+        text = "Modified!";
+        Console.WriteLine($"Number: {number}, Text: {text}");
+    }
+}
+
---
+++ palindrome/palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program()
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine("Enter a word to check if it is a palindrome");
+        //string? word=Console.ReadLine();
+        //if(word != null)
+        //    Checkpalindrome(word);
+       
+        
+
+        Console.WriteLine("Enter factorial number");
+        int n = int.Parse(Console.ReadLine());
+
+        // Ensure n is not 0 before proceeding
+        if (n != 0)
+        {
+            int x = factorial(n);  // Correct way to declare within the if block
+            Console.WriteLine($"Factorial of {n} is {x}");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a non-zero number for factorial calculation.");
+        }
+    }
+    public static void Checkpalindrome(string palindrome)
+    {
+        string? reverse = palindrome.Trim().ToLower().Reverse().ToString();
+        if (reverse == palindrome.Trim().ToLower())
+            Console.WriteLine("This is a palindrome");
+        else
+            Console.WriteLine("This is not a palindrome");
+    }
+
+    public static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+}
+
